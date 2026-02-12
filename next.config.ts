e613bf442dffed27210e295b972a7a9a51d1a301
unknown <mouchine3@gmail.com>
2026-02-12 10:35:32 +0100

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: true,
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Disables Image Optimization API
+  },
 };
 
 export default nextConfig;
